ec4ebe591a013852172fded657f44571aae6564d
JesseNHLStenden <jesse.van.der.voet@student.nhlstenden.com>
2024-05-28 15:53:36 +0200

    feat: added live update of token at launch

--- pocketbase/pocketbaseClient.js
+++ pocketbase/pocketbaseClient.js
@@ -1,21 +1,42 @@
 const PocketBase = require('pocketbase/cjs');
 const BASE_URL = process.env.BASE_URL;
+const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 class PocketBaseClient {
-    constructor() {
-      if (!PocketBaseClient.instance) {
-        this.client = new PocketBase(BASE_URL);
-        PocketBaseClient.instance = this;
-      }
-      return PocketBaseClient.instance;
+  constructor() {
+    if (!PocketBaseClient.instance) {
+      this.client = new PocketBase(BASE_URL);
+      PocketBaseClient.instance = this;
     }
-  
-    getClient() {
-      return this.client;
+    return PocketBaseClient.instance;
+  }
+
+  async loginAsAdmin() {
+    try {
+      await this.client.admins.authWithPassword(ADMIN_EMAIL, ADMIN_PASSWORD);
+      console.log('Admin login successful');
+    } catch (error) {
+      console.error('Admin login failed:', error);
     }
   }
-  
-  const instance = new PocketBaseClient();
-  Object.freeze(instance);
-  
-  module.exports = instance.getClient();
+
+  async logoutAdmin() {
+    try {
+      await this.client.admins.authRefresh();
+      this.client.authStore.clear();
+      console.log('Admin logout successful');
+    } catch (error) {
+      console.error('Admin logout failed:', error);
+    }
+  }
+
+  getClient() {
+    return this.client;
+  }
+}
+
+const instance = new PocketBaseClient();
+Object.freeze(instance);
+
+module.exports = instance;
--- src/server.js
+++ src/server.js
@@ -1,33 +1,52 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
+const { createHash } = require('crypto');
+
+const pocketbaseClient = require('../pocketbase/pocketbaseClient');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const DEV_ENV = process.env.DEV_ENV === "DEVELOPMENT";
-const DATABASE_PUBLIC_HASH = process.env.DATABASE_PUBLIC_HASH || "random_test_hash";
 
-const requestAuthentication = require('../middleware/requestAuthentication')();
+try {
+    await pocketbaseClient.loginAsAdmin();
 
-const apiRoutes = require('../routes/test_auth_requests');
+    const oldHash = await pocketbaseClient.getClient().collection('api').getList(1, 1, { sort: '-created' });
 
-app.use('/api', requestAuthentication);
+    const data = {
+        "hash": createHash('sha256').update(oldHash['items'][0]['hash'] + oldHash['items'][0]['id']).digest('hex')
+    };
 
-app.use('/api', apiRoutes);
+    const record = await pocketbaseClient.getClient().collection('api').create(data);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    console.log(record);
 
-app.listen(PORT, () => {
-    if (DEV_ENV) {
-        console.log("Detected development environment!");
-    }
+    const requestAuthentication = require('../middleware/requestAuthentication')(record['hash']);
 
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    const apiRoutes = require('../routes/test_auth_requests');
 
+    app.use('/api', requestAuthentication);
 
-// String formatting
-// const test_string = "{\"response\": \"Het Concertgebouw, located in Amsterdam, Netherlands, is one of the world's most renowned concert halls, celebrated for its exceptional acoustics and rich history. Opened in 1888, the venue is home to the Royal Concertgebouw Orchestra and hosts a wide array of performances, including classical music, jazz, and world music. The building's design, by architect Adolf Leonard van Gendt, features a neoclassical facade and an elegantly decorated interior, making it a cultural landmark in Amsterdam. With over 900 concerts annually, Het Concertgebouw attracts visitors and performers from all over the globe, contributing significantly to the city's cultural heritage.\"}"
+    app.use('/api', apiRoutes);
 
-// console.log(test_string.replace('\"', '"'))
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(PORT, () => {
+        if (DEV_ENV) {
+            console.log("Detected development environment!");
+        }
+
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+
+}
+catch (error) {
+    console.log(error)
+}
+finally {
+    await pocketbaseClient.logoutAdmin(); 
+}
